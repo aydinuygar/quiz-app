ce1b7189c5f47b1bf073309cd0a9e7e5e4ce2f14
uygar <uygarang@gmail.com>
2024-07-19 17:13:55 +0300

    edit quiz table seeder

--- database/seeders/QuizTableSeeder.php
+++ database/seeders/QuizTableSeeder.php
@@ -5,28 +5,34 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Quiz;
 use App\Models\Question;
+use Faker\Factory as Faker;
 
 class QuizTableSeeder extends Seeder
 {
     public function run()
     {
-        // Quiz oluştur
-        $quiz = Quiz::create([
-            'title' => 'Sample Quiz',
-            'description' => 'This is a sample quiz.'
-        ]);
+        $faker = Faker::create();
 
-        // Quiz'e ait 10 adet Question oluştur
-        for ($i = 1; $i <= 10; $i++) {
-            Question::create([
-                'quiz_id' => $quiz->id,
-                'question_text' => "Question $i?",
-                'option1' => "Option A for Question $i",
-                'option2' => "Option B for Question $i",
-                'option3' => "Option C for Question $i",
-                'option4' => "Option D for Question $i",
-                'correct_option' => "Option A" // Örnek olarak her sorunun doğru cevabını A olarak atıyoruz
+        for ($j = 1; $j <= 3; $j++) { // 3 Quiz oluştur
+            // Quiz oluştur
+            $quiz = Quiz::create([
+                'title' => $faker->sentence,
+                'description' => $faker->paragraph,
+                'time_limit' => 10 // dakika cinsinden zaman limiti
             ]);
+
+            // Quiz'e ait 10 adet Question oluştur
+            for ($i = 1; $i <= 10; $i++) {
+                Question::create([
+                    'quiz_id' => $quiz->id,
+                    'question_text' => $faker->sentence . "?", // Faker ile rastgele soru cümlesi
+                    'option1' => $faker->word, // Faker ile rastgele kelime
+                    'option2' => $faker->word, // Faker ile rastgele kelime
+                    'option3' => $faker->word, // Faker ile rastgele kelime
+                    'option4' => $faker->word, // Faker ile rastgele kelime
+                    'correct_option' => "Option A" // Örnek olarak her sorunun doğru cevabını A olarak atıyoruz
+                ]);
+            }
         }
     }
 }
